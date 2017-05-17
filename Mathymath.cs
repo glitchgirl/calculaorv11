210ff3358e365d6663ffe5ef8d4afab3de6120e2
Morgan Smith <msmith1270@augusta.edu>
2017-05-17 12:57:24 -0400

    Add files via upload

---
+++ Mathymath.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Mathymath
+    {
+            decimal CurrentValue;
+            decimal SelectedValue;
+            decimal Output;
+        public decimal PerformCalculation()
+        {
+            Console.Write("please input first value");
+            string input = Console.ReadLine();
+            Decimal.TryParse(input, out CurrentValue);
+            Console.Write("Please input second value");
+            string changing = Console.ReadLine();
+            Decimal.TryParse(changing, out SelectedValue);
+            Console.Write("Please input what you want to do");
+            string math = Console.ReadLine();   
+
+                //addition use key + or add
+                if (math == "+" || math == "add")
+                    CurrentValue = CurrentValue + SelectedValue;
+
+                //subtraction use key - or subtract
+                else if (math == "-" || math == "subtract")
+                    CurrentValue = CurrentValue - SelectedValue;
+
+                //multipication use key * or multiply 
+                else if (math == "*" || math == "multiply")
+                    CurrentValue = CurrentValue * SelectedValue;
+
+                //division use key / or divide
+                else if (math == "/" || math == "divide")
+                {
+                    if (SelectedValue != 0)
+                        CurrentValue = CurrentValue / SelectedValue;
+                    else
+                    {
+                        Console.Write("Can't divide by zero");
+                        while (SelectedValue == 0)
+                        {
+                            Console.Write("Please put in second input");
+                            string DontBdumb = Console.ReadLine();
+                            Decimal.TryParse(DontBdumb, out SelectedValue);
+                        }
+                        CurrentValue = CurrentValue / SelectedValue;
+                    }
+                }
+
+                // clear to clear 
+                else if (math == "clear")
+                {
+                    CurrentValue = 0;
+                    SelectedValue = 0;
+                }
+
+                //what was typed doesn't make sense
+                else
+                {
+                    Console.Write("That's not an input");
+                }
+
+            //write answer
+                Output = CurrentValue;
+                Console.Write("Output:{0}", Output);
+                return Output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt
+            Console.WriteLine("Welcome to the best calculator");
+            Console.WriteLine("Please type numbers using characters not words. 1 not one");
+            Console.WriteLine("Addition: use key + or word add");
+            Console.WriteLine("Subtraction: use key - or word subtract");
+            Console.WriteLine("Multiplication: use key * or word multiply");
+            Console.WriteLine("Division: use key / or word divide");
+            Console.WriteLine("Clear: clear");
+            Console.WriteLine("Final answer: out");
+
+            
+            var instance = new Mathymath();
+            instance.PerformCalculation();
+
+            Console.WriteLine("Do you want to do another calculation?");
+            string go = Console.ReadLine();
+            if (go == "Y")
+            {
+                var instanceRepeat = new Mathymath();
+                instanceRepeat.PerformCalculation();
+            }
+            Console.WriteLine("PAKC");
+            Console.Read();
+        }//main
+    }//program
+}
